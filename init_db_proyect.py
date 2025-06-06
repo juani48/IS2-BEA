341517633403e7aa681aedb51917a59c006f47f9
Laradev88 <laradevincenti@hotmail.com>
2025-06-06 08:44:35 -0300

    base de datos con nuestros datos

--- init_db_proyect.py
+++ init_db_proyect.py
@@ -32,78 +32,38 @@ def __CATEGORIAS__():
 def __MAQUINAS__():
     AddMachine.usecase_add_machine(
         patent="ABC1", 
-        mark="Marca ABC1", 
-        model="Modelo ABC1", 
-        price_day=10_500, 
-        ubication="Ubicacion ABC1", 
+        mark="CAT", 
+        model="cargadora", 
+        price_day=100_000, 
+        ubication="Avenida 1 Calle 80", 
         refund=10, 
-        categorie=["Jardineria"], 
-        description="Descripcion de la maquina ABC1",
-        creation_date="0001-1-1")
+        categorie=["Construccion"], 
+        description="Ideal para cargar",
+        creation_date="1985-5-1")
 
     AddMachine.usecase_add_machine(
         patent="ABC2", 
-        mark="Marca ABC2", 
-        model="Modelo ABC2", 
-        price_day=30_500, 
+        mark="CAT", 
+        model="Excavadora", 
+        price_day=10_000, 
         ubication="Ubicacion ABC2", 
         refund=10, 
         categorie=["Construccion"], 
-        description="Descripcion de la maquina ABC2",
-        creation_date="0001-1-1")
+        description="Ideal para excavar",
+        creation_date="2000-10-5")
         
-    AddMachine.usecase_add_machine(
-        patent="123A", 
-        mark="Marca 123A", 
-        model="Modelo 123A", 
-        price_day=27_750, 
-        ubication="Ubicacion 123A", 
-        refund=15, 
-        categorie=["Jardineria"], 
-        description="Descripcion de la maquina 123A",
-        creation_date="0001-1-1")
-    
-    AddMachine.usecase_add_machine(
-        patent="C1AB", 
-        mark="Marca C1AB", 
-        model="Modelo C1AB", 
-        price_day=13_500, 
-        ubication="Ubicacion C1AB", 
-        refund=5, 
-        categorie=["Mineria"], 
-        description="Descripcion de la maquina C1AB",
-        creation_date="0001-1-1")
+ 
 
-    AddMachine.usecase_add_machine(
-        patent="N2CA", 
-        mark="Marca N2CA", 
-        model="Modelo N2CA", 
-        price_day=40_500, 
-        ubication="Ubicacion N2CA", 
-        refund=5, 
-        categorie=["Construccion"], 
-        description="Descripcion de la maquina N2CA",
-        creation_date="0001-1-1")
         
-    AddMachine.usecase_add_machine(
-        patent="75TW", 
-        mark="Marca 75TW", 
-        model="Modelo 75TW", 
-        price_day=50_750, 
-        ubication="Ubicacion 75TW", 
-        refund=18, 
-        categorie=["Transporte"], 
-        description="Descripcion de la maquina 75TW",
-        creation_date="0001-1-1")
     
 def __ADMIN__():
     admin = UserModel(
             dni=11222333,
             email="bobelalquilador@gmail.com",
-            name="ADMIN",
+            name="Bob",
             lastname="ADMIN",
-            phone=0000,
-            birth_date="0001-1-1",
+            phone=555555555,
+            birth_date="1975-1-1",
             password="ADMIN",
             employee_number=-1,
             type="Admin",
@@ -112,107 +72,79 @@ def __ADMIN__():
     query_TEST_USER.execute(user=admin)
 
 def __USUARIOS__():
-    carlos = UserModel(
-        dni=44555666, # ----> Cambiar el DNI de Carlos a 66555444
-        email="carlosmartinez@gmail.com",
-        name="Carlos",
-        lastname="Martinez",
-        phone=0000,
-        birth_date="0001-2-1",
-        password="contraseña 123",
+    Juliana = UserModel(
+        dni=49087890,
+        email="ju@gmail.com",
+        name="Juliana",
+        lastname="Perez",
+        phone=12345678,
+        birth_date="2004-6-28",
+        password="contraseña123",
         employee_number=0,
         type="Cliente",
         authorized=True)
-    query_TEST_USER.execute(user=carlos) 
+    query_TEST_USER.execute(user=Juliana) 
 
-    usuario_con_puntos = UserModel(
-        dni=43907096,
-        email="usuario_con_puntos@gmail.com",
-        name="Usuario",
-        lastname="Con Puntos",
-        phone=0000,
-        birth_date="0001-3-1",
-        password="UsuarioConPuntos",
-        employee_number=0,
-        type="User",
-        authorized=True)
-    query_TEST_USER.execute(user=usuario_con_puntos)
-    query_update_user_points.execute(dni=43907096, number=10)
-
-    usuario_sin_puntos = UserModel(
-        dni=45872513,
-        email="usuario_sin_puntos@gmail.com",
-        name="Usuario",
-        lastname="Sin Puntos",
-        phone=0000,
-        birth_date="0001-4-1",
-        password="usuarioSinPuntos",
-        employee_number=0,
-        type="User",
-        authorized=True)
-    query_TEST_USER.execute(user=usuario_sin_puntos)
-    query_update_user_points.execute(dni=45872513, number=4)
 
     jose = UserModel(
-        dni=47654123,
+        dni=48654123,
         email="josepereyra@gmail.com",
-        name="Jose",
-        lastname="Fina",
-        phone=0000,
-        birth_date="0001-5-1",
-        password="JoseFina",
+        name="Josefina",
+        lastname="Pereyra",
+        phone=123456788,
+        birth_date="2004-6-28",
+        password="josefinita1",
         employee_number=0,
-        type="User",
+        type="Cliente",
         authorized=True)
     query_TEST_USER.execute(user=jose)
-
-    juan = UserModel(
-        dni=45400389,
-        email="jibyrab@gmail.com",
-        name="Ju",
-        lastname="An",
-        phone=0000,
-        birth_date="0001-6-1",
-        password="JuAn",
-        employee_number=0,
-        type="User",
-        authorized=True)
-    query_TEST_USER.execute(user=juan)
+    query_update_user_points.execute(dni=48654123, number=4)
 
 def __EMPLEADOS__():
-    empleado1 = UserModel(
-        dni=99888777,
-        email="empleado99888777@gmail.com",
-        name="Empleado",
-        lastname="1",
-        phone=0000,
-        birth_date="0001-5-1",
-        password="empleado1",
+    empleadoFacu = UserModel(
+        dni=88433627,
+        email="empleado88433627@gmail.com",
+        name="Facu",
+        lastname="lopez",
+        phone=11111111,
+        birth_date="2002-5-15",
+        password="pepito123",
         employee_number=1234,
         type="Empleado",
         authorized=True)
-    query_TEST_USER.execute(user=empleado1)
+    query_TEST_USER.execute(user=empleadoFacu)
 
-    empleado2 = UserModel(
-        dni=44555667,
-        email="empleado44555667@gmail.com",
-        name="Empleado",
-        lastname="2",
-        phone=0000,
-        birth_date="0001-6-1",
+    empleadoNahu = UserModel(
+        dni=44555666,
+        email="nahuHiriart@gmail.com",
+        name="Nahuel",
+        lastname="Hiriart",
+        phone=25836914,
+        birth_date="2003-11-5",
         password="empleado2",
-        employee_number=4321,
+        employee_number=4444,
         type="Empleado",
         authorized=True)
-    query_TEST_USER.execute(user=empleado2)
+    query_TEST_USER.execute(user=empleadoNahu)
+
+    empleadoLara = UserModel(
+        dni=74156741,
+        email="laradevincenti@hotmail.com",
+        name="Lara",
+        lastname="Devin",
+        phone=25836914,
+        birth_date="2002-5-1",
+        password="larita",
+        employee_number=8888,
+        type="Empleado",
+        authorized=True)
+    query_TEST_USER.execute(user=empleadoLara)
 
 def __RESERVAS__():
     AddReservation.init_usecase_add_reserve(
         start_day="2025-5-2",
-        #start_day=datetime.datetime(2025, 5, 2, 0, 0, 0),
         end_day="2025-5-9",
-        #end_day=datetime.datetime(2025, 5, 9, 0, 0, 0),
-        client_id=44555666, # ----> RESERVA DE CARLOS
+        client_id=49087890, # ----> RESERVA DE Juliana
         machine_id="ABC1",
         shipment=False,
         type="Cliente",
@@ -220,11 +152,9 @@ def __RESERVAS__():
     )
 
     AddReservation.init_usecase_add_reserve(
-        start_day="2025-6-2",
-        #start_day=datetime.datetime(2025, 6, 2, 0, 0, 0),
-        end_day="2025-6-9",
-        #end_day=datetime.datetime(2025, 6, 9, 0, 0, 0),
-        client_id=44555666, # ----> RESERVA DE CARLOS
+        start_day="2025-6-6",
+        end_day="2025-6-15",
+        client_id=48654123, # ----> RESERVA DE Jose
         machine_id="ABC1",
         shipment=False,
         type="Cliente",
@@ -232,23 +162,9 @@ def __RESERVAS__():
     )
 
     AddReservation.init_usecase_add_reserve(
-        start_day="2025-7-2",
-        #start_day=datetime.datetime(2025, 7, 2, 0, 0, 0),
-        end_day="2025-7-9",
-        #end_day=datetime.datetime(2025, 7, 9, 0, 0, 0),
-        client_id=44555666, # ----> RESERVA DE CARLOS
-        machine_id="ABC1",
-        shipment=False,
-        type="Cliente",
-        apply_discount=False
-    )
-
-    AddReservation.init_usecase_add_reserve(
-        start_day="2025-6-2",
-        #start_day=datetime.datetime(2025, 6, 2, 0, 0, 0),
-        end_day="2025-6-9",
-        #end_day=datetime.datetime(2025, 6, 9, 0, 0, 0),
-        client_id=44555666, # ----> RESERVA DE CARLOS
+        start_day="2025-9-20",
+        end_day="2025-10-3",
+        client_id=48654123, # ----> RESERVA DE Jose
         machine_id="ABC2",
         shipment=False,
         type="Cliente",
@@ -256,23 +172,9 @@ def __RESERVAS__():
     )
 
     AddReservation.init_usecase_add_reserve(
-        start_day="2025-7-3",
-        #start_day=datetime.datetime(2025, 7, 2, 0, 0, 0),
-        end_day="2025-7-9",
-        #end_day=datetime.datetime(2025, 7, 9, 0, 0, 0),
-        client_id=44555666, # ----> RESERVA DE CARLOS
-        machine_id="123A",
-        shipment=True,
-        type="Cliente",
-        apply_discount=False
-    )
-
-    AddReservation.init_usecase_add_reserve(
-        start_day="2025-6-2",
-        #start_day=datetime.datetime(2025, 6, 2, 0, 0, 0),
-        end_day="2025-6-9",
-        #end_day=datetime.datetime(2025, 6, 9, 0, 0, 0),
-        client_id=45400389, # ----> RESERVA DE JUAN
+        start_day="2025-11-2",
+        end_day="2025-11-14",
+        client_id=49087890, # ----> RESERVA DE Juliana
         machine_id="ABC2",
         shipment=False,
         type="Cliente",
@@ -280,17 +182,17 @@ def __RESERVAS__():
     )
 
     AddReservation.init_usecase_add_reserve(
-        start_day="2025-7-2",
-        #start_day=datetime.datetime(2025, 7, 2, 0, 0, 0),
-        end_day="2025-7-9",
-        #end_day=datetime.datetime(2025, 7, 9, 0, 0, 0),
-        client_id=45400389, # ----> RESERVA DE JUAN
+        start_day="2025-8-10",
+        end_day="2025-8-25",
+        client_id=48654123, # ----> RESERVA DE Jose
         machine_id="ABC2",
         shipment=True,
         type="Cliente",
         apply_discount=False
     )
 
+    
+    
 
 
 

510ea4147a7a509122652b6f7e810894e5f081d1
Juani <jibyrab@gmail.com>
2025-06-06 12:14:48 -0300

    Si

--- data/query/disable/query_disable_machine.py
+++ data/query/disable/query_disable_machine.py
@@ -1,7 +1,9 @@
 from data.config import session
 from data.model.MachineModel import MachineModel
+from data.model.ReservationModel import ReservationModel
 
 def execute(patent):
     local_machine = session.get(MachineModel, patent)
     local_machine.disable = True
+    session.query(ReservationModel).filter(ReservationModel.machine_id == patent).delete()
     session.commit()

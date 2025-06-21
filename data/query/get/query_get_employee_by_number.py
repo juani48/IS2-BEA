9f1d6504b69600be4639a4432689e3e464a8f13d
priscilapotulnisky <potulniskyp@gmail.com>
2025-06-21 16:53:23 -0300

    bloqueo del calendario

--- data/query/get/query_get_employee_by_number.py
+++ data/query/get/query_get_employee_by_number.py
@@ -1,5 +1,11 @@
 from data.config import session
 from data.model.UserModel import UserModel
+from sqlalchemy import or_
 
 def execute(employeeN):
-    return session.query(UserModel).filter(UserModel.employee_number == employeeN).first()
+    return session.query(UserModel).filter(
+        or_(
+            UserModel.employee_number == employeeN,
+            UserModel.employee_number == -employeeN
+        )
+    ).first()

dca6e29015d406c36b4d1d4948f3bdc87e68c3e9
Laradev88 <laradevincenti@hotmail.com>
2025-06-27 11:53:00 -0300

    arreglo

--- data/query/insert/query_insert_maintenance.py
+++ data/query/insert/query_insert_maintenance.py
@@ -3,15 +3,9 @@ from data.config import session
 from data.model.MaintenanceModel import MaintenanceModel
 
 def execute(start_day, client_id, start_employee_id, machine_id, maintenance):
-<<<<<<< HEAD
     local_maintenance = (
     session.query(MaintenanceModel)
     .filter(
-=======
-    local_maintenance = session.query(
-        MaintenanceModel
-    ).filter(
->>>>>>> 94125584ee4fcf2145d4b3959cfc5c9699d6041f
         and_(
             MaintenanceModel.start_day == start_day,
             MaintenanceModel.client_id == client_id,

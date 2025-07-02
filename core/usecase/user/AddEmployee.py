ec01f062d628b5fa6c975d54071cc948ac2356b0
Juani <jibyrab@gmail.com>
2025-07-02 08:37:55 -0300

    Insertar empleado
    Agrege la corroboraciones que faltaban al CU

--- core/usecase/user/AddEmployee.py
+++ core/usecase/user/AddEmployee.py
@@ -2,11 +2,16 @@ from data.model.UserModel import UserModel
 from data.appDataBase import insert_employee
 from core.usecase.user.RequestUser import _random_password
 from core.usecase.service import SendMail
-from data.query.get import query_get_user
+from data.query.get import query_get_user, query_get_employee, query_get_user_by_email
 
 def usecase_add_employee(dni, email, name, lastname, phone, dateBirth, employeeN):
     if query_get_user.execute(dni):
-        raise Exception("El DNI ya se encuentra registrado")
+        raise Exception("El DNI ya se encuentra registrado.")
+    if query_get_employee.execute(dni) or query_get_employee.execute(dni * -1):
+        raise Exception("El numero de empleado ya se encuntra registrado.")
+    if query_get_user_by_email.execute(email=email):
+        raise Exception("El correo ya se encuntra registrado.")
+
     if (_validator(dni, email, name, lastname)):        
         password = _random_password()
         user = UserModel(
--- data/query/get/query_get_user.py
+++ data/query/get/query_get_user.py
@@ -4,6 +4,4 @@ from data.model.UserModel import UserModel
 
 def execute (dni):
     local_user = session.get(UserModel,dni)
-    if local_user == None:
-        raise Exception("El usuario no existe.")
     return local_user

330472fae919b1b306d685271ec8cc7728237e82
Juani <jibyrab@gmail.com>
2025-07-03 20:49:42 -0300

    AA

--- core/usecase/reserve/MachineReservations.py
+++ core/usecase/reserve/MachineReservations.py
@@ -2,4 +2,5 @@ from data.appDataBase import get_all_reservations_by_machine
 
 def usecase_get_all_reservations_by_machine(machine_id):
     list = get_all_reservations_by_machine(machine_id)
+    print(list)
     return [ x.json_days() for x in list ]
--- data/appDataBase.py
+++ data/appDataBase.py
@@ -9,7 +9,7 @@ from data.query.delete import query_delete_mc, query_delete_user
 
 from data.query.enable import query_enable_categorie, query_enable_machine, query_enable_user,query_enable_employee
 
-from data.query.get_all import query_get_all_commentarys, query_get_all_machines, query_get_all_machines_by_categorie, query_get_all_employees, query_get_all_users, query_get_all_categories, query_get_all_reservations_by_machine,query_get_all_requests,query_get_all_reservation,query_get_all_reservations_by_dni, query_get_all_reservations_by_dni,query_get_all_disable_employees, query_get_all_machines_admin, query_get_all_machines_by_categorie_admin, query_get_all_maintenance,query_get_all_questions, query_get_all_rent_by_date, query_get_all_reseration_by_date, query_get_all_rent_by_categorie, query_get_all_reseration_by_categorie
+from data.query.get_all import query_get_all_commentarys, query_get_all_machines, query_get_all_machines_by_categorie, query_get_all_employees, query_get_all_users, query_get_all_categories, query_get_all_reservations_by_machine,query_get_all_requests,query_get_all_reservation,query_get_all_reservations_by_dni, query_get_all_reservations_by_dni,query_get_all_disable_employees, query_get_all_machines_admin, query_get_all_machines_by_categorie_admin, query_get_all_maintenance,query_get_all_questions, query_get_all_rent_by_date, query_get_all_reseration_by_date, query_get_all_rent_by_categorie, query_get_all_reseration_by_categorie, query_get_all_rents_by_dni
 
 from data.query.insert import query_insert_user, query_insert_machine, query_insert_categorie, query_insert_mc,query_insert_employee, query_insert_reserve, query_TEST_USER, query_insert_rent, query_insert_maintenance,query_insert_question,query_insert_commentary
 
@@ -180,6 +180,9 @@ def get_all_reservations():
 
 def get_all_reservations_by_dni(client_id):
     return query_get_all_reservations_by_dni.execute(client_id)
+
+def get_all_rents_by_dni(client_id):
+    return query_get_all_rents_by_dni.execute(client_id)
 
 def get_all_maintenance():
     return query_get_all_maintenance.execute()
